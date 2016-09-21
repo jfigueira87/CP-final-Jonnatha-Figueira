7b2c7295bef87eeb1fbee569e8305d10f58974b2
Jonnatha Figueira <jfigueira@gmail.com>
2016-09-21 13:58:08 +0200

    Método restar() añadiendo un mensaje de error

--- Calculadora.java
+++ Calculadora.java
@@ -8,9 +8,14 @@ public class Calculadora {
               
     }
     
-    public static int multiplicar(double a, double b){
-        int s=a*b;
-        return s;
+    public static int restar(int a, int b){
+		if (a<b){
+			System.out.println("Error. El primer número debe ser mayor para poder realizar la resta");
+		}else{
+			int s=a-b;
+			return s;
+		}
+        
     }
     
     
