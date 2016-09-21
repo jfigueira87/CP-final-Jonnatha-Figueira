2d707e5d52647dc1cb22621a8829d8267b180fde
Jonnatha Figueira <jfigueira@gmail.com>
2016-09-21 13:46:43 +0200

    Método restar() de la clase Calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -8,8 +8,8 @@ public class Calculadora {
               
     }
     
-    public static int sumar(int a, int b){
-        int s=a+b;
+     public static int restar(int a, int b){
+        int s=a-b;
         return s;
     }
     
