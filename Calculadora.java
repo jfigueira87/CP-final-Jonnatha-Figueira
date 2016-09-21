7c19a5823f2f7f4455b6e7fdb01e8d19e8e5bb27
Jonnatha Figueira <jfigueira@gmail.com>
2016-09-21 13:39:19 +0200

    Método sumar() de la clase Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,17 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+              
+    }
+    
+    public static int sumar(int a, int b){
+        int s=a+b;
+        return s;
+    }
+    
+    
+}
